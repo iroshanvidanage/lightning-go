82c6ba6e6db1763bf2a36c14afb6f9b2740286c1
iroshanav <knightshift.kurosaki@gmail.com>
2025-04-10 00:22:49 +0530

    add basic overview of go

---
+++ start-go/main.go
@@ -0,0 +1,11 @@
+// The first line here declares the project/workspace/package the script belongs to
+package main
+
+// Import modules and other libraries
+import "fmt"
+
+// The main package should contain a function called main
+// This is how a function/method is defined
+func main() {
+	fmt.Println("Hi there!")
+}
